91c8d3d119dc8bb1fde6fe5d2db900fedc756a4c
Kiirill-NiK <nikoslkiy.kirill.andreevich@gmail.com>
2013-03-26 21:36:57 +0400

    added README.txt added testpathes, ~testignore осталась проблема с точным подсчетом доков.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <QDir>
-#include <stdio.h>
 #include <QString>
 #include <QFile>
+#include <stdio.h>
 #include <QRegExp>
 #include <QtDebug>
 #include <QTime>
 
-unsigned int totalTestingFunCount;
-unsigned int totalVirtualCount;
-unsigned int totalDocumentedCount;
+unsigned int totalTestingFunCount = 0;
+unsigned int totalVirtualCount = 0;
+unsigned int totalDocumentedCount = 0;
 
 QStringList ignoreFiles;
+QString output;
+QString outputStats;
+QStringList testpathes;
 
 QString commentFreeString(QString str)
 {
-	int docCount = str.count(QString("///"));
-	docCount += str.count(QString("/**"));
+	int docCount = str.count(QString("/// ")); // this counting is used because we have style guide
+	docCount += str.count(QString("/** "));
+	docCount += str.count(QString("//! "));
 	totalDocumentedCount += docCount;
 	QRegExp reg = QRegExp("/\\*.*\\*/");
 	reg.setMinimal(true);
@@ -34,17 +38,32 @@ unsigned int macrosesWithParameters(QString str)
 	return str.count(QRegExp("Q_[A-Z]+(\\s)*\\([^\\)]*\\)"));
 }
 
+bool isIgnored(QString fileName)
+{
+	bool isMatch = false;
+	for (int j = 0; j < ignoreFiles.length(); ++j)
+	{
+		QRegExp exReg(".*" + ignoreFiles.at(j) + ".*");
+		if (exReg.exactMatch(fileName)) {
+			isMatch = true;
+			output.append("~" + fileName + "\n");
+			break;
+		}
+	}
+	return isMatch;
+}
+
 unsigned int localFunctionCount(QString path, QString fileName)
 {
 	QFile file(path + fileName + ".h");
 	// the second condition need for events when .h is interface
-	if (!file.open(QIODevice::ReadOnly | QIODevice::Text) || !QFile::exists(path + fileName + ".cpp")) {
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text) || !QFile::exists(path + fileName + ".cpp")
+		|| isIgnored(path + fileName + ".h")) {
 		return 0;
 	}
 
 	QString header = file.readAll();
 	header = commentFreeString(header);
-	//qDebug() << header;
 
 	QRegExp reg("[^A-Za-z0-9~_]~?[_A-Za-z0-9]+(\\s)*\\([^;]*;");
 	unsigned int virtCount = virtualMethodsCount(header);
@@ -54,43 +73,46 @@ unsigned int localFunctionCount(QString path, QString fileName)
 	return localCount;
 }
 
+// without interface methods
 void totalFunctionCount(QString dir)
 {
 	QString filter = "*.h";
 	QDir directory(dir);
 	QStringList fileList = directory.entryList(QDir::Files);
-	QStringList filtFileList = fileList.filter(QRegExp(filter, Qt::CaseInsensitive, QRegExp::Wildcard));
-	for (int i = 0; i < filtFileList.length(); ++i)
+	QStringList filterFileList = fileList.filter(QRegExp(filter, Qt::CaseInsensitive, QRegExp::Wildcard));
+	unsigned int localCount = 0;
+	unsigned int oldDocumentedCount = totalDocumentedCount;
+	for (int i = 0; i < filterFileList.length(); ++i)
 	{
-		QString file = filtFileList.at(i);
+		QString file = filterFileList.at(i);
 		file.chop(2);
-		totalTestingFunCount += localFunctionCount(dir, file);
+		localCount += localFunctionCount(dir, file);
 	}
+	totalTestingFunCount += localCount;
 	QStringList dirList = directory.entryList(QDir::AllDirs | QDir::NoDotAndDotDot);
-	qDebug() << dir << "\ntesting: " << totalTestingFunCount << "\n";
+	output.append(dir + "\n\ttesting methods: " + QString::number(localCount) + "\tdocumented: " + QString::number(totalDocumentedCount - oldDocumentedCount) + "\n");
 	for (int i = 0; i < dirList.length(); ++i)
 	{
 		bool isMatch = false;
 		for (int j = 0; j < ignoreFiles.length(); ++j)
 		{
 			QRegExp exReg(".*" + ignoreFiles.at(j) + ".*");
-			//exReg.setMinimal(true);
-			if (exReg.exactMatch(dirList.at(i))) {
+			if (exReg.exactMatch(dir + dirList.at(i) + "/")) {
 				isMatch = true;
-				qDebug() << "!!!" << dir + dirList.at(i) + "/";
+				output.append("~" + dir + dirList.at(i) + "/\n");
 				break;
 			}
 		}
 		if (isMatch) {
-			break;
+			continue;
 		}
 		totalFunctionCount(dir + dirList.at(i) + "/");
 	}
 }
 
-void fillIgnoreFiles()
+void fillIgnoredFiles()
 {
-	QFile file("~files.txt");
+	QFile file("~testignore");
 	if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
 		return;
 	}
@@ -101,7 +123,32 @@ void fillIgnoreFiles()
 		QString line = in.readLine();
 		ignoreFiles.append(line);
 	}
-	qDebug() << ignoreFiles;
+	file.close();
+}
+
+void fillPathesForTest()
+{
+	QFile file("testpathes");
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		return;
+	}
+
+	QTextStream in(&file);
+	while (!in.atEnd())
+	{
+		QString line = in.readLine();
+		testpathes.append(line);
+	}
+	file.close();
+}
+
+void fillLog()
+{
+	output.prepend("\nignored pathes:\n" + ignoreFiles.join("\n") + "\n----------\n\n");
+	outputStats.append("\n\t\ttesting: " + QString::number(totalTestingFunCount) + "\n");
+	outputStats.append("\t\tvirtual: " + QString::number(totalVirtualCount) + "\n");
+	outputStats.append("\t\ttotal: " + QString::number(totalVirtualCount + totalTestingFunCount) + "\n");
+	outputStats.append("\t\tdocumented: " + QString::number(totalDocumentedCount) + "\n\n");
 }
 
 int main(int argc, char *argv[])
@@ -109,14 +156,27 @@ int main(int argc, char *argv[])
 	QTime time;
 	time.start();
 
-	fillIgnoreFiles();
-	// without interface methods
-	totalFunctionCount(QString("D:/QReal/qreal/"));
-	qDebug() << "virtual: " << totalVirtualCount << "\n";
-	qDebug() << "total: " << totalVirtualCount + totalTestingFunCount << "\n";
-	qDebug() << "documented: " << totalDocumentedCount << "\n";
+	fillIgnoredFiles();
+	fillPathesForTest();
+	for (int j = 0; j < testpathes.length(); ++j)
+	{
+		totalFunctionCount(testpathes.at(j));
+		fillLog();
+		outputStats.prepend(testpathes.at(j) + "\n");
+		QFile outputFile("log" + QString::number(j) + ".txt");
+		if (!outputFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+			return 0;
+		}
+		QTextStream out(&outputFile);
+		out << outputStats << output;
+		totalTestingFunCount = 0;
+		totalVirtualCount = 0;
+		totalDocumentedCount = 0;
+		outputStats.clear();
+		output.clear();
+		outputFile.close();
+	}
 
-	qDebug("time of execution: %d ms", time.elapsed());
-
+	qDebug() << "time of execution: " + QString::number(time.elapsed()) + " ms";
 	return 0;
 }
